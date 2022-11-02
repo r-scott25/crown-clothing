fd2a25c010a7355702f2bcaa562f28f04ec1a3d1
r-scott25 <rachelbernal2@gmail.com>
2022-11-02 14:50:30 -0500

    added styling using scss

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import "./categories.styles.scss";
+
 const App = () => {
   const categories = [
     {
@@ -23,8 +25,8 @@ const App = () => {
   ];
   return (
     <div className="categories-container">
-      {categories.map(({ title }) => (
-        <div className="category-container">
+      {categories.map(({ title, id }) => (
+        <div key={id} className="category-container">
           <div className="background-image" />
           <div className="category-body-container">
             <h2>{title}</h2>
